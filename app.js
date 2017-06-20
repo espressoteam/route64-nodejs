14b1f80747672c7774de0c992ffc1e34bdad133c
Khongsak Kawdettikhun <k.khongsak@gmail.com>
2017-06-21 01:12:48 +0700

    Add /api/route endpoint.

--- app.js
+++ app.js
@@ -6,11 +6,11 @@ const serviceAccount = require('./route64-cecb6-firebase-adminsdk-tqabj-e79b4556
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: 'https://route64-cecb6.firebaseio.com'
-});
+})
 
-app.get('/', function (req, res) {
+app.put('/api/route/:id', function (req, res) {
   // temporary hardcoded token
-  const registrationToken = 'e0Q1cu9E0Cs:APA91bHbhWjYMOWbwCqRCvIOoNUnCIlCWo4gS7mTV709R1LNPbt90UAMafmoPTCV0fyZatk8PeE9YIEE7mZiQ-oAsedDsAPBSaRU6KO2bGsCUctRU6kxj54yb9EuqbQLW6cLpx3E8--V'
+  const registrationToken = 'eChFXwTvDHo:APA91bHM36FjAe_d_Bq_eipPkhJpoDJ9x3zw1ay67X5kp2-l8tK_etc0aXgQvVpT1f85so-OZrwtS5SJCTk0W0aNQAKdNCJMZKtFvSf-zZnko0X0yWLpCc7QdqhNJBzb-yhDOAXJypKo'
   var payload = {
     data: {
       score: '850',
@@ -19,10 +19,10 @@ app.get('/', function (req, res) {
   }
   admin.messaging().sendToDevice(registrationToken, payload)
     .then(function (response) {
-      console.log('Successfully sent message:', response);
+      console.log('Successfully sent message:', response)
     })
     .catch(function (error) {
-      console.log('Error sending message:', error);
+      console.log('Error sending message:', error)
     })
   res.send('Hello World!')
 })
